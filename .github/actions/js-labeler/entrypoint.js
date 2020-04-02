356f8c3228f6d44c1f01bdde8149121d7f52704a
Chris Reid <creid@wyzant.com>
2020-04-02 13:04:18 -0500

    MOre change

--- .github/actions/js-labeler/entrypoint.js
+++ .github/actions/js-labeler/entrypoint.js
@@ -5,11 +5,15 @@ async function run() {
     const github_token = core.getInput('github_token');
     const octokit = new github.GitHub(github_token);
     const payload = github.context.payload;
-    octokit.issues.addLabels({
+    let response = octokit.issues.addLabels({
         owner: payload.repository.owner,
         repo: payload.repository.name,
         issue_number: payload.issue.number,
-        labels: "test-label",
+        labels: ["test-label"],
+    }).then((success) => {
+        console.log("Success", success);
+    }).catch((err) => {
+        console.log("error", err);
     });
 }
 
